90ee5cc9648bf8689851ab58756128dea58f1089
xzx-dalao <1340796532@qq.com>
2021-09-25 02:48:28 +0800

    更换api

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,6 @@
-// const proBaseURL_163 = "http://119.23.233.90:4000";
-// const devBaseURL_163 = "http://119.23.233.90:4000";
-const proBaseURL_163 = "http://123.207.32.32:9001";
-const devBaseURL_163 = "http://123.207.32.32:9001";
-// const devBaseURL_163 = "http://39.102.36.212:3000";
-// const proBaseURL_163 = "http://39.102.36.212:3000";
+const proBaseURL_163 = "http://cloud-music.pl-fe.cn";
+const devBaseURL_163 = "http://cloud-music.pl-fe.cn";
+
 
 
 export const BASE_URL_163 = process.env.NODE_ENV === 'development' ? devBaseURL_163 : proBaseURL_163;
